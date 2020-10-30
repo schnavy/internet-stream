e28e4d0ccf03286a79447db0b44d83f55ca90e5b
David <>
2020-10-30 22:19:51 +0100

    minor changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,23 @@
-const body = document.querySelector("body")
+//Preloader
+
+let imgArray = [];
+for (i = 0; i < data["image-urls"].length; i++) {
+  imgArray[i] = new Image();
+  imgArray[i] = data["image-urls"][i];
+}
+
+const body = document.querySelector("body");
 let currImg = document.querySelector("#curr-img");
-let imgArray = data["image-urls"];
+// let imgArray = data["image-urls"];
 let clickInfo = document.querySelector("#clickInfo");
 let n = 0;
+let scrollCounter = 0;
 let streamIsActive = false;
 let stream = null;
 
 function changeImg() {
-  currImg.src = imgArray[n];
-  console.log(imgArray[n]);
+  let r = Math.floor(Math.random() * imgArray.length);
+  currImg.src = imgArray[r];
 
   if (n == imgArray.length - 1) {
     n = 0;
@@ -18,16 +27,27 @@ function changeImg() {
 }
 
 window.addEventListener("click", (e) => {
-  currImg.style.display = "block"
+  currImg.style.display = "block";
   if (streamIsActive) {
     clearInterval(stream);
-    clickInfo.style.display ="block"
-    body.style.backgroundColor = "red"
-    } else {
-    stream = setInterval(changeImg, 500);
-    clickInfo.style.display ="none"
-    body.style.backgroundColor = "grey"
+    currImg.classList.remove("fullscreen");
+    clickInfo.style.display = "block";
+    body.style.backgroundColor = "red";
+  } else {
+    currImg.classList.add("fullscreen");
+    stream = setInterval(changeImg, 100);
+    clickInfo.style.display = "none";
+    body.style.backgroundColor = "grey";
+
   }
   streamIsActive = !streamIsActive;
-  console.log(streamIsActive);
+});
+
+window.addEventListener("scroll", (e) => {
+  currImg.style.display = "block";
+  if (scrollCounter % 5 == 0) {
+    changeImg();
+  }
+  scrollCounter++;
+  console.log(e);
 });
